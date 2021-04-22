3e4b84100cc0b23746f89e9739245b85e94345ea
Furkan Kurt <furkan.kurt.13@outlook.com>
2021-04-22 21:37:04 +0300

    last touch

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,30 +5,30 @@ import Todo from './Todo';
 function TodoList() {
   const [todos, setTodos] = useState([]);
 
+
   const addTodo = todo => {
     if (!todo.text || /^\s*$/.test(todo.text)) {
       return;
     }
-
     const newTodos = [todo, ...todos];
-
     setTodos(newTodos);
     console.log(...todos);
   };
+
 
   const updateTodo = (todoId, newValue) => {
     if (!newValue.text || /^\s*$/.test(newValue.text)) {
       return;
     }
-
     setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
   };
 
+
   const removeTodo = id => {
     const removedArr = [...todos].filter(todo => todo.id !== id);
-
     setTodos(removedArr);
   };
+
 
   const completeTodo = id => {
     let updatedTodos = todos.map(todo => {
@@ -40,6 +40,7 @@ function TodoList() {
     setTodos(updatedTodos);
   };
 
+  
   return (
     <>
       <h1>What's the Plan for Today?</h1>
